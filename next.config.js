cc085913bf07be54640fd24fe40876c7aea9ee79
Mattias de Hollander <m.dehollander@nioo.knaw.nl>
2020-12-30 13:16:37 +0100

    Add locale setting, lang attribute to html element

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 
 module.exports = {
+  i18n: {
+    locales: ['nl-NL'],
+    defaultLocale: 'nl-NL'
+  },
   env: {
     GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     BASE_REPO_FULL_NAME: process.env.BASE_REPO_FULL_NAME,
