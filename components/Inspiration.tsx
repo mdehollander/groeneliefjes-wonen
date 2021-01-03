487fbd36ab5b056a575e735dd15142366d00621b
Mattias de Hollander <m.dehollander@nioo.knaw.nl>
2021-01-03 20:41:58 +0100

    use inline image

--- components/Inspiration.tsx
+++ components/Inspiration.tsx
@@ -55,7 +55,16 @@ function SetupInspiration({ data, index }:any) {
     <BlocksControls index={index}>
         <div className="xl:w-1/3 md:w-1/2 p-4">
           <div className="bg-gray-100 p-6 rounded-lg">
-            <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content" />
+              <InlineImage
+                name="image"
+                parse={media => `/img/${media.filename}`}
+                uploadDir={() => '/public/img/'}
+                focusRing={false}
+              >
+                {props => (
+                    <Image loading="lazy" className="h-40 rounded w-full object-cover object-center mb-6" src={props.src as string} alt="place" width="600" height="400" />
+                )}
+              </InlineImage>
             <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
                 <BlockTextarea
                   name="title"
