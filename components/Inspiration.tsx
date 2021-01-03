b6c3c20871b1262e6c3ac03146a5a55c720687d4
Mattias de Hollander <m.dehollander@nioo.knaw.nl>
2021-01-03 14:37:09 +0100

    Fix block template

--- components/Inspiration.tsx
+++ components/Inspiration.tsx
@@ -22,30 +22,7 @@ export default function Inspiration() {
                 <div className="w-16 h-1 rounded-full bg-green-500 inline-flex"></div>
               </div>
             </div>
-            <div className="flex flex-wrap -m-4">
-              <InlineBlocks className="flex flex-wrap -m-4" name="inspiration.places" blocks={SETUP_INSPIRATION}/>
-              <div className="xl:w-1/3 md:w-1/2 p-4">
-                <div className="bg-gray-100 p-6 rounded-lg">
-                  <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content" />
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Dorothy Gemeenschap</h2>
-                  <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                </div>
-              </div>
-              <div className="xl:w-1/3 md:w-1/2 p-4">
-                <div className="bg-gray-100 p-6 rounded-lg">
-                  <Image className="h-40 rounded w-full object-cover object-center mb-6" loading="lazy" src="/img/noelhuis.png" alt="noelhuis" width="600" height="400" />
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Jeannette Noëlhuis</h2>
-                  <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                </div>
-              </div>
-              <div className="xl:w-1/3 md:w-1/2 p-4">
-                <div className="bg-gray-100 p-6 rounded-lg">
-                  <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content" />
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">De Wonne</h2>
-                  <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-                </div>
-              </div>
-            </div>
+            <InlineBlocks className="flex flex-wrap -m-4" name="inspiration.places" blocks={SETUP_INSPIRATION}/>
           </div>
         </section>
     </>
@@ -53,7 +30,7 @@ export default function Inspiration() {
 }
 
 const setup_inspiration_template: BlockTemplate = {
-  label: 'Core Values',
+  label: 'Places',
   defaultItem: {
     title: 'Inspiring place',
     description: 'description',
@@ -64,7 +41,7 @@ const setup_inspiration_template: BlockTemplate = {
 }
 
 const SETUP_INSPIRATION = {
-  setup_point: {
+  setup_place: {
     Component: SetupInspiration,
     template: setup_inspiration_template,
   },
@@ -72,7 +49,7 @@ const SETUP_INSPIRATION = {
 
 
 
-function SetupInspiration({ index }:any) {
+function SetupInspiration({ data, index }:any) {
   return (
     <>
     <BlocksControls index={index}>
