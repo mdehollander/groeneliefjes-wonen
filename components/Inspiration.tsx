37b47ff0cf2c32de64faa3ba18ae17e68ef88532
Mattias de Hollander <m.dehollander@nioo.knaw.nl>
2021-01-10 11:00:32 +0100

    Add places

--- components/Inspiration.tsx
+++ components/Inspiration.tsx
@@ -50,11 +50,11 @@ const SETUP_INSPIRATION = {
 
 
 
-function SetupInspiration({ index }:any) {
+function SetupInspiration({ data, index }:any) {
   return (
     <>
     <BlocksControls index={index}>
-        <div className="xl:w-1/3 md:w-1/2 p-4">
+        {/*<div className="xl:w-1/3 md:w-1/2 p-4">
           <div className="bg-gray-100 p-6 rounded-lg">
               <InlineImage
                 name="image"
@@ -81,6 +81,28 @@ function SetupInspiration({ index }:any) {
                 />
             </p>
           </div>
+        </div>
+        */}
+        <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
+          <a className="h-full flex items-center border-gray-200 border p-4 rounded-lg" href={data.link}>
+            <img alt="place" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src={data.image} />
+            <div className="flex-grow">
+              <h2 className="text-gray-900 title-font font-medium">
+                  <BlockTextarea
+                    name="title"
+                    placeholder="Place"
+                    focusRing={false}
+                  />
+              </h2>
+              <p className="text-gray-600">
+                  <BlockTextarea
+                    name="description"
+                    placeholder="Description"
+                    focusRing={false}
+                  />
+              </p>
+            </div>
+        </a>
         </div>
     </BlocksControls>
     </>
