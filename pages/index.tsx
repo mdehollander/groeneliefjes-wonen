5f73eb6801ad58cc4b26f0ca095aeb1cb5be0f4d
Mattias de Hollander <m.dehollander@nioo.knaw.nl>
2021-02-28 11:12:30 +0100

    Fix imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,6 @@ import Hero from "../components/Hero"
 import About from "../components/About"
 import Features_content from '../components/Features-content'
 import Inspiration from '../components/Inspiration'
-import Testimonial from '../components/Testimonial'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
 import Kernwaarden from "../components/Kernwaarden"
